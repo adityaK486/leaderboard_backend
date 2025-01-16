43205740c4e3a8e2e49a15f68742fd888ff12f9f
Aditya Kumar <100521099+adityaK486@users.noreply.github.com>
2025-01-17 03:46:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,6 +16,9 @@ app.use(cors({
 app.use(express.json());
 
 app.use('/api/users',userRoutes);
+app.use((req, res, next) => {
+  res.status(404).send('Page Not Found');
+});
 
 app.listen(PORT,()=>{
     connectMongoDB();
