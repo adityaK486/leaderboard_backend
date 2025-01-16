d47cd68c6a2bd50b54c19619e385daed97c2fec5
Aditya Kumar <100521099+adityaK486@users.noreply.github.com>
2025-01-17 03:25:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ const PORT = process.env.PORT||5000;
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: 'https://grand-fox-402735.netlify.app', 
+}));
 app.use(express.json());
 
 app.use('/api/users',userRoutes);
